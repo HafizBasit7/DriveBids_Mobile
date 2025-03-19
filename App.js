df73ee351f1b3f8f209f5b9b3c8431373542020b
Rehan <ranarehan9x@gmail.com>
2025-03-20 02:05:49 +0500

    update

--- App.js
+++ App.js
@@ -2,8 +2,7 @@ import useLoadFonts from "./Hooks/useLoadFonts";
 import R1_App from "./R1_App";
 import AuthContextProvider from "./R1_Contexts/authContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import AdDetails from "./Screens/UmairScreens/AdDetails";
-import MyAdDetails from "./Screens/UmairScreens/MyAdDetails";
+
 const queryClient = new QueryClient();
 
 export default function App() {
@@ -11,10 +10,10 @@ export default function App() {
   if (!fontsLoaded) {
     return null;
   }
-  return (
+  return (    
     <AuthContextProvider>
       <QueryClientProvider client={queryClient}>
-        <R1_App />
+        <R1_App/>
       </QueryClientProvider>
     </AuthContextProvider>
   );
--- Navigations/StackNavigation.js
+++ Navigations/StackNavigation.js
@@ -2,22 +2,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { useAuth } from "../R1_Contexts/authContext";
 import R1_AuthStack from "./R1_AuthStack";
 import R1_DashboardStack from "./R1_DashboardStack";
-import R1_HomeStack from "./R1_HomeStack";
 
 const AppNavigator = () => {
-  const { authState } = useAuth();
+
+  const {authState} = useAuth();
 
   return (
     <NavigationContainer>
-      {authState.isAuthenticated ? (
-        authState.isHome ? (
-          <R1_HomeStack />
-        ) : (
-          <R1_DashboardStack />
-        )
-      ) : (
-        <R1_AuthStack />
-      )}
+      {authState.isAuthenticated ?  <R1_DashboardStack/> : <R1_AuthStack/>}
     </NavigationContainer>
   );
 };
