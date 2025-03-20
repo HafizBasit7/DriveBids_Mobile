897b7d9713287b7d5bc07b6dc7102c9d23cf06a4
AyzennTech <168638793+AyzennTech@users.noreply.github.com>
2025-03-20 22:07:13 +0500

    Home Stack Done

--- Navigations/R1_HomeStack.js
+++ Navigations/R1_HomeStack.js
@@ -1,26 +1,58 @@
 import { createStackNavigator } from "@react-navigation/stack";
 
-//Header
+// Headers
 import HomeHeader from "../CustomComponents/UmairComponents/Homeheader";
-//AdDetails
+import Header from "../CustomComponents/UmairComponents/Header"; // Your second header
+
+// Screens
+import Home from "../Screens/Tahir-Screens/Home/Home";
 import AdDetails from "../Screens/UmairScreens/AdDetails";
 import ReserveMet from "../Screens/UmairScreens/ReserveMet";
 import MyAdDetails from "../Screens/UmairScreens/MyAdDetails";
 import PlaceBid from "../Screens/UmairScreens/PlaceBid";
+import FiltersScreen from "../Screens/Tahir-Screens/FiltersScreen/Filters";
 const Stack = createStackNavigator();
 
 export default function R1_HomeStack() {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        header: () => <HomeHeader />, // Default header for all screens
-      }}
-    >
-      {/* Sell Car */}
-      <Stack.Screen name="AdDetails" component={AdDetails} />
-      <Stack.Screen name="MyAdDetails" component={MyAdDetails} />
-      <Stack.Screen name="PlaceBid" component={PlaceBid} />
-      <Stack.Screen name="ReserveMet" component={ReserveMet} />
+    <Stack.Navigator>
+      {/* Home Screen with "Header" */}
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          header: () => <Header />,
+        }}
+      />
+      <Stack.Screen
+        name="FiltersScreen"
+        component={FiltersScreen}
+        options={{
+          header: () => <Header />,
+        }}
+      />
+
+      {/* Other Screens with "HomeHeader" */}
+      <Stack.Screen
+        name="AdDetails"
+        component={AdDetails}
+        options={{ header: () => <HomeHeader /> }}
+      />
+      <Stack.Screen
+        name="MyAdDetails"
+        component={MyAdDetails}
+        options={{ header: () => <HomeHeader /> }}
+      />
+      <Stack.Screen
+        name="PlaceBid"
+        component={PlaceBid}
+        options={{ header: () => <HomeHeader /> }}
+      />
+      <Stack.Screen
+        name="ReserveMet"
+        component={ReserveMet}
+        options={{ header: () => <HomeHeader /> }}
+      />
     </Stack.Navigator>
   );
 }
