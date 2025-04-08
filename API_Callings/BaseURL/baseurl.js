f5010e59e5600f4be1f593e3b834b66dd0ea9a4b
sudo-tahirsaeed <sudotahirsaeed@gmail.com>
2025-04-08 18:37:38 +0500

    updated links back to render

--- API_Callings/BaseURL/baseurl.js
+++ API_Callings/BaseURL/baseurl.js
@@ -1,2 +1,2 @@
 // export const baseurl = "https://drivebids-backend-v2.onrender.com";
-export const baseurl = "http://srv694651.hstgr.cloud/service1";
+export const baseurl = "https://srv694651.hstgr.cloud/service1";
--- API_Callings/BaseURL/chat-baseurl.js
+++ API_Callings/BaseURL/chat-baseurl.js
@@ -1,2 +1,3 @@
-// export const chatBaseUrl = "https://drivebids-messageserver-web-v2.onrender.com";
-export const chatBaseUrl = "http://srv694651.hstgr.cloud/service2";
+export const chatBaseUrl =
+  "https://drivebids-messageserver-web-v2.onrender.com";
+// export const chatBaseUrl = "https://srv694651.hstgr.cloud/service2";
