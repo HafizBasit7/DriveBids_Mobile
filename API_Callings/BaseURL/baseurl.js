af1265ab1a37f937fc215f8e3222a1520a39e036
sudo-tahirsaeed <sudotahirsaeed@gmail.com>
2025-06-01 12:00:40 +0500

    updated base url to prod

--- API_Callings/BaseURL/baseurl.js
+++ API_Callings/BaseURL/baseurl.js
@@ -1,4 +1,2 @@
-// export const baseurl = "https://srv694651.hstgr.cloud/drivebidapi";
- export const    baseurl= "http://192.168.1.19:3020"
-
-
+export const baseurl = "https://srv694651.hstgr.cloud/drivebidapi";
+//  export const    baseurl= "http://192.168.1.19:3020"
